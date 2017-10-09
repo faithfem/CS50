6461fc8c979e0309bc5274071f3fb7cff57f12f9
faithfem <fmugure@intracitygeeks.org>
2017-10-09 15:31:10 -0400

    first commit

---
+++ week1/app.js
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) {
+  printf("hello, world\n");
+}
+
+for (var i = 0; i < 50; i++) {
+  printf("hello, world\n");
+}
+
+if(x < y){
+  printf("x is greater than y\n");
+}else if (x > y){
+  printf("x is greater than y\n");
+}else {
+  printf("x is equal to y\n");
+}
+
+//ARRAYS
